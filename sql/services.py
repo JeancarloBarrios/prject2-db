37b97742764aa0edf061dcb7fc78468c45a36dbb
derekolsson <derekolsson@gmail.com>
2016-05-12 17:34:22 -0600

    insert, create, delete

--- sql/services.py
+++ sql/services.py
@@ -9,3 +9,20 @@ def _get_session():
         return 'error'
 
     return conn.cursor()
+
+def _insert_client(nombre, type, phone, url, email, addr, city, netvalue, clientsince, twitteracct, pic)
+    cur = _get_session()
+    query= 'INSERT INTO Clientes (id_cliente, nombre, cliente_type, telefono, sitio_web, correo_electronico, direccion, ciudad, valor_net, cliente_desde, twitter_acct, foto) VALUES (nextval(val_id_cliente),'+name+ ',' +type+ ',' +phone + ','+ url + ',' + email +','+ addr+ ','+city+','+netvalue+','+clientsince+','+twitteracct+','+pic+';')
+    cur.execute(query)
+
+
+def _update_client(campos[], oldVals[], newVals[]):
+    cur = _get_session()
+    for i in campos.length:
+        query= 'UPDATE Clientes SET '+campos[i]+'= '+newVals[i]+' WHERE '+campos[i]+' = '+oldVals[i]+';'
+        cur.execute(query)
+
+def _delete_client(id):
+    cur = _get_session()
+    query = 'DELETE FROM Clientes WHERE id_cliente='+id+';'
+    cur.execute(query)
