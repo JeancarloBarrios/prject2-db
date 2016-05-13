3032628b584cecdb08cfb08e77b1c1e3666c506c
derekolsson <derekolsson@gmail.com>
2016-05-12 18:04:56 -0600

    fixed queries again

--- sql/services.py
+++ sql/services.py
@@ -10,19 +10,19 @@ def _get_session():
 
     return conn.cursor()
 
-def _insert_client(nombre, type, phone, url, email, addr, city, netvalue, clientsince, twitteracct, pic)
+def insert_client(nombre, tipo, phone, url, email, addr, city, netvalue, clientsince, twitteracct, pic):
     cur = _get_session()
-    query= 'INSERT INTO Clientes (id_cliente, nombre, cliente_type, telefono, sitio_web, correo_electronico, direccion, ciudad, valor_net, cliente_desde, twitter_acct, foto) VALUES (nextval(val_id_cliente),'+name+ ',' +type+ ',' +phone + ','+ url + ',' + email +','+ addr+ ','+city+','+netvalue+','+clientsince+','+twitteracct+','+pic+';')
+    query= 'INSERT INTO Clientes (id_cliente, nombre, cliente_type, telefono, sitio_web, correo_electronico, direccion, ciudad, valor_net, cliente_desde, twitter_acct, foto) VALUES (nextval(val_id_cliente),"%s","%d","%s","%s","%s","%s","%s","%d","%s","%s","%s");',name,tipo,phone,url,email,addr,city,netvalue,clientsince,twitteracct,pic
     cur.execute(query)
 
 
-def _update_client(campos[], oldVals[], newVals[]):
+def update_client(campos[], oldVals[], newVals[]):
     cur = _get_session()
     for i in campos[]:
-        query= 'UPDATE Clientes SET '+campos[i]+'= '+newVals[i]+' WHERE '+campos[i]+' = '+oldVals[i]+';'
+        query= 'UPDATE Clientes SET %s = "%s" WHERE %s = "%s";',campos[i], newVals[i],campos[i],oldVals[i]
         cur.execute(query)
 
-def _delete_client(id):
+def delete_client(id):
     cur = _get_session()
-    query = 'DELETE FROM Clientes WHERE id_cliente='+id+';'
+    query = 'DELETE FROM Clientes WHERE id_cliente="%s";',id
     cur.execute(query)
